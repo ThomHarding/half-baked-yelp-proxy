53b29b52824e94682d0cc7e5ae848b0c9f1a136c
THOMAS HARDING <hardingt604@gmail.com>
2022-05-17 17:43:18 -0600

    changed yelp and pokemon fetches to use the right urls. also begone tests

--- functions/yelp.js
+++ functions/yelp.js
@@ -9,6 +9,13 @@ const headers = {
 
 exports.handler = async (event, context) => {
   try {
+    const response = await fetch(`https://api.yelp.com/v3/businesses/search?location=${event.queryStringParameters.searchFilter}`, {
+      headers: {
+        'Authorization': `Bearer ${process.env.YELP_KEY}`,
+      },
+    });
+    const data = await response.json();
+    const json = JSON.stringify({ data });
     // grab the city, state, and country from the request's query parameters
     // here is an example from the netlify docs:
     // https://functions.netlify.com/playground/#hello%2C-%7Bname%7D 
@@ -21,7 +28,7 @@ exports.handler = async (event, context) => {
       statusCode: 200, 
       headers,
     // this is where you shoot data back to the user. right now it's sending an empty object--replace this with the yelp data. remember, you do need to stringify it, otherwise netlify gets mad. ¯\_(ツ)_/¯
-      body: JSON.stringify({}),
+      body: json
     };
   } catch (error) {
     console.log(error);
