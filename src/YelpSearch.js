e65aaa98e2e980b8a8d6441423773fdde25cef3b
THOMAS HARDING <hardingt604@gmail.com>
2022-05-17 20:10:55 -0600

    yelpsearch.js tracks location instead of city, state, and country

--- src/YelpSearch.js
+++ src/YelpSearch.js
@@ -6,13 +6,11 @@ import BusinessList from './BusinessesList.js';
 export default function YelpSearch() {
   const [yelpSearch, setYelpSearch] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [cityName, setCityName] = useState('Chicago');
-  const [country, setCountry] = useState('');
-  const [state, setState] = useState('');
+  const [location, setLocation] = useState('Chicago');
       // you'll need to track your yelp search results, the loading state, and one form field: name. For this form field, set a real initial values (like 'pikachu') so the form populates with a default value.
 
   async function load() {
-    const results = await getYelpData(cityName);
+    const results = await getYelpData(location);
     setYelpSearch(results.data.businesses);
     // put the jsonified data in state and set the loading state to false
     setLoading(false);
@@ -34,10 +32,8 @@ export default function YelpSearch() {
     <section className='yelp'>
       {/* make the fetch on submit */}
       <form onSubmit={handleYelpSubmit}>
-        Search yelp for a city
-        <input value={cityName} onChange={e => setCityName(e.target.value)}/>
-        <input value={country} onChange={e => setCountry(e.target.value)}/>
-        <input value={state} onChange={e => setState(e.target.value)}/>
+        Search yelp for a city 
+        <input value={location} onChange={e => setLocation(e.target.value)}/>
         {/* add inputs/labels for city name, state, and country, using all the things we need with react forms. Don't forget to use the value property to sync these up with the default values in react state */}
         <button>Get yelp</button>
       </form>
